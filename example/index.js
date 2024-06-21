b5f42af51f2f5d708f347eb243ecab0026b483f2
Ali Ahmad <mian41935@gmail.com>
2024-06-21 16:41:14 +0500

    update example code add:comments: #2

--- example/index.js
+++ example/index.js
@@ -1,4 +1,11 @@
-const { getRandomQuote, getDailyQuote } = require('zen-quotes');
+const { getQuotes,getRandomQuote, getDayOfQuote } = require('zen-quotes');
 
-console.log(getRandomQuote());
-console.log(getDailyQuote('programming'));
+// Example usage:
+    const quotes = getQuotes({category: 'programming', numberOfQuotes:1, auther: true});
+    console.log(`Here's a quote by "${quotes.author}": "${quotes.text}"\n`);  // will return quote with author [optional] , numberOfQuotes [optional] and category [required]; 
+
+    const randomQuote = getRandomQuote('education');
+    console.log(`Here's a quote by "${randomQuote.author}": "${randomQuote.text}"\n`); // will return quote with random quote :  category[required]
+
+    const dayOfQuote = getDayOfQuote();
+    console.log(`Here's a quote by "${dayOfQuote.author}": "${dayOfQuote.text}"\n`); // will return quote with day of quote
